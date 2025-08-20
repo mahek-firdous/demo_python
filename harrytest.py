586b21f844bef3dc7e5a8bd73855eaa141db7081
Mahek Firdous <mahekfirdous1234@gmail.com>
2025-08-20 21:34:20 +0530

    Add files via upload

---
+++ harrytest.py
@@ -0,0 +1,11 @@
+import time
+timestamp=time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp=int(time.strftime('%H'))
+print(timestamp)
+timestamp=time.strftime('%M')
+print(timestamp)
+timestamp=time.strftime('%SS')
+
+
+
